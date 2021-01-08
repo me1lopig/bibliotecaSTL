225fd65e8427bd6b736846784ca7de96ec4d35a9
Germán López Pineda <me1lopig@uco.es>
2021-01-08 01:02:14 +0100

    20210108_2

--- complejos/complejos.cpp
+++ complejos/complejos.cpp
@@ -5,14 +5,14 @@
 
 #include <iostream>
 #include<complex>
-#include<vector>
+
 
 using namespace std;
 
 
 int main()
 {
-    // declaarcion de variables 
+    // declaarcion de variables de declaracion de numeros complejos
     complex<double>c1,c2;
     c1 = complex<double>(3.3, 4.7);
     c2 = complex<double>(-2., 8);
@@ -20,6 +20,10 @@ int main()
     // operaciones con complejos
     cout << "Suma de complejos " << c1 << "+" << c2 << "=" << c1 + c2 << endl;
     cout << "Resta de complejos " << c1 << "-" << c2 << "=" << c1 - c2 << endl;
+
+    cout << "Producto de complejos " << c1 << "*" << c2 << "=" << c1 * c2 << endl;
+    cout << "Division de complejos " << c1 << "/" << c2 << "=" << c1 / c2 << endl;
+
     cout << "La parte real de " << c1 << " es " << real(c1) << endl;
     cout << "La parte imaginaria de " << c1 << " es " << imag(c1) << endl;
 
